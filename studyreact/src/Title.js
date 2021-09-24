0e991fb33584a55af58e93b926867a5f3d41dc81
sh414lim <77113306+sh414lim@users.noreply.github.com>
2021-09-24 17:35:18 +0900

    ?

--- studyreact/src/Title.js
+++ studyreact/src/Title.js
@@ -4,7 +4,7 @@ import React from "react";
 function Title(){
     return(
     <div className="title">
-        {/* <h1>{this.props.title}</h1> */}
+        <h1>{this.props.title}</h1>
         <p>hello study</p>
     </div>        
     )
