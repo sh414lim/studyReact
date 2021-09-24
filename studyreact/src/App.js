1eaa140e2bde3e8ac9f3af0d1e0bfbf7031286af
sh414lim <77113306+sh414lim@users.noreply.github.com>
2021-09-24 17:31:13 +0900

    props

--- studyreact/src/App.js
+++ studyreact/src/App.js
@@ -1,8 +1,22 @@
+import React  from "react";
+import Subject from "./Subject";
+import Title from "./Title";
+function TOC(){
+  return(
+    <div>
+    <li><a href="1.html">web</a></li>
+    <li><a href="2.html">react</a></li>
+    </div>
+  )
+}
+
 
 function App() {
   return (
-    <div>
-      <h1>hello world</h1>
+    <div className="App">
+      <Title title="web" sub="what props?"/>
+      <TOC/>
+      <Subject/>
     </div>
   );
 }
---
+++ studyreact/src/Subject.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+class Subject extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>Hello WEB</h1>
+            </div>
+        )
+    }
+}
+
+export default Subject;
